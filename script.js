a8884b9fa21a6437412f3aecd2bebff2012a31a1
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-01-20 03:24:23 +0530

    feat: add UI to the rpc game

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let computerScore = 0;
+let buttons = document.querySelectorAll(".btn");
+let hScoreNode = document.querySelector(".h-score");
+let bScoreNode = document.querySelector(".b-score");
+let roundInfo = document.querySelector(".round-info");
+let botScore = 0;
 let humanScore = 0;
 
-// get computer choice
-function getComputerChoice() {
+// get bot choice
+function getBotChoice() {
 	let randomNumber = Math.random();
 
 	if (randomNumber <= 0.33) {
@@ -14,65 +18,33 @@ function getComputerChoice() {
 	}
 }
 
-// get human choice
-function getHumanChoice() {
-	// when press esc or cancel prompt it returns null, returns empty string if user press ok without entering in input
-	let input = prompt("Enter between rock/paper/scissors: ");
-
-	if (input === null) {
-		return null;
-	}
-
-	input = input.toLowerCase();
-	if (input === "rock" || input === "paper" || input === "scissors") {
-		return input;
-	} else {
-		alert("Error: Invalid input. Enter between rock/paper/scissors");
-
-		// add return for recursive calling. read below to understand
-		// When you call a function recursively, you need to ensure that each recursive call returns a value back to the original caller. Otherwise, the original function call won't get the result from the subsequent call.
-		return getHumanChoice();
-	}
-}
-
 //play 1 round
-function playRound(humanChoice, computerChoice) {
-	if (humanChoice === computerChoice) {
-		alert(
-			`Draw! Both choose ${humanChoice}.\nYour score: ${humanScore}.\nComputer score: ${computerScore}.`
-		);
+function playRound(humanChoice, botChoice) {
+	if (humanChoice === botChoice) {
+		roundInfo.innerHTML = `<p>You choose ${humanChoice}, Bot choose ${botChoice}</p>
+							<p>Its a <strong>DRAW!<strong/></p>`;
 	} else if (
-		(humanChoice === "rock" && computerChoice === "scissors") ||
-		(humanChoice === "paper" && computerChoice === "rock") ||
-		(humanChoice === "scissors" && computerChoice === "paper")
+		(humanChoice === "rock" && botChoice === "scissors") ||
+		(humanChoice === "paper" && botChoice === "rock") ||
+		(humanChoice === "scissors" && botChoice === "paper")
 	) {
-		alert(`You win! ${humanChoice} beats ${computerChoice}.\nYour score: ${++humanScore}.\nComputer score: ${computerScore}.`);
+		humanScore++;
+		roundInfo.innerHTML = `<p>You choose ${humanChoice}, Bot choose ${botChoice}.</p>
+							<p>${humanChoice} beats ${botChoice}.</p>
+							<p>You <strong>WIN!<strong/></p>`;
 	} else {
-		alert(`You lose! ${computerChoice} beats ${humanChoice}.\nYour score: ${humanScore}.\nComputer score: ${++computerScore}`);
+		botScore++;
+		roundInfo.innerHTML = `<p>You choose ${humanChoice}, Bot choose ${botChoice}</p>
+							<p>${botChoice} beats ${humanChoice}.</p>
+							<p>You <strong>Loose!<strong/></p>`;
 	}
-}
-// playRound(getHumanChoice(), getComputerChoice());
 
-function playGame() {
-	for (let i = 1; i <= 5; i++) {
-		const computerChoice = getComputerChoice();
-		const humanChoice = getHumanChoice();
-
-		// if human choice null then don't call playRound
-		if (humanChoice) {
-			playRound(humanChoice, computerChoice);
-		} else {
-			alert("Game Cancelled!");
-			return;
-		}
-	}
+	hScoreNode.textContent = `Your Score: ${humanScore}`;
+	bScoreNode.textContent = `Bot Score: ${botScore}`;
 }
 
-playGame();
-if (humanScore === computerScore) {
-	alert(`And the winner is ...... drum rolls .......No one!`);
-} else if (humanScore > computerScore) {
-	alert(`And the winner is ...... drum rolls .......You!\nYou won!!`);
-} else {
-	alert(`And the winner is ...... drum rolls .......Computer!\nYou loose :(`);
-}
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		playRound(button.id, getBotChoice());
+	});
+});
